6eb06562b87685f985eb061a4cb3c4505e117a70
Simon Lomax <simax.99.99@gmail.com>
2016-11-20 11:13:24 +0000

    Added web.config to enable running app on azure

--- GithubMockApiServer/GithubMockApiServer.fs
+++ GithubMockApiServer/GithubMockApiServer.fs
@@ -1,13 +1,15 @@
 open System
 open Suave
+open Suave.Web
 open Suave.Operators
 open Suave.Successful
 open Suave.RequestErrors
 open Suave.Filters
 open System.IO
 
+
 [<EntryPoint>]
-let main argv = 
+let main [| port |] = 
     let json fileName =
         let content = File.ReadAllText fileName  
         content.Replace("\r", "").Replace("\n","")
@@ -17,8 +19,15 @@ let main argv =
     let repos = pathScan "/users/%s/repos" (fun _ -> "Repos.json" |> json)
     // let aList = ["One";"Two"]
     // let home = GET >=> path "/" >=> request (fun r -> (OK (sprintf "using: %s @ %s %s" r.host (string DateTime.Now) (aList.Head)))) 
-    let home = GET >=> path "/" >=> request (fun r -> (OK (sprintf "using: %s @ %s" r.host (string DateTime.Now)))) 
+    // let home = GET >=> path "/" >=> request (fun r -> (OK (sprintf "using: %s @ %s" r.host (string DateTime.Now)))) 
+    let home = GET >=> path "/" >=> request (fun r -> (OK "Hello AZURE !!!!!!!")) 
     let mockApi = choose [repos;user; home; BAD_REQUEST "Oooops unknown route"]
 
-    startWebServer defaultConfig mockApi          
+    let config =
+            { defaultConfig with
+                bindings = [ HttpBinding.mk HTTP Net.IPAddress.Loopback (uint16 port) ]
+                listenTimeout = TimeSpan.FromMilliseconds 3000.
+            }
+
+    startWebServer config mockApi          
     0 
